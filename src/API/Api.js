43e775d0afa294cc9a66d7a06f722f1c31fbdb8e
pollchowdhury <84124457+pollchowdhury@users.noreply.github.com>
2022-10-03 17:31:41 -0400

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,23 +2,6 @@ import axios from "axios";
 import List from "../List/List";
 
 // Google  Places API Search
-const GoogleAPIKey = "AIzaSyAF9i1A12NtujwP1WXmYgZd7d-lbE20iY8";
-export const GetGooglePlacesData = async () => {
-  var axios = require("axios");
-  var config = {
-    method: "get",
-    url: "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=YOUR_API_KEY",
-    headers: {},
-  };
-
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
 
 // Foursquare Places API Search
 export const GetFoursquarePlacesData = async (l1, l2, service) => {
