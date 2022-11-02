ba9216f86493ecdd3073d50b2241c8cb6497ee98
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-02 17:56:07 -0400

    downloaded antd dependency

--- src/Components/TireAlignment/index.js
+++ src/Components/TireAlignment/index.js
@@ -3,6 +3,7 @@ import RecommendedList from "../List/List_recommend";
 import NearbyList from "../List/List_nearby";
 import { GetNearbyPlaces, GetRecommendedPlaces } from "../../API/Api";
 import { Titleholder } from "./alignment";
+import Insightsalignment from "../InsightsSections/InsightsAlignment";
 
 export default function Alignment() {
   let service = "Alignment";
