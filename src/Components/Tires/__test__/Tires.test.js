184de878ae7ca27865e420971ed569e5f302d90c
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-30 05:25:32 -0500

    Added Unit test for the tires places method

--- src/Components/Tires/__test__/Tires.test.js
+++ src/Components/Tires/__test__/Tires.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
 import Tires from "../index";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 it("Tires page renders correctly", () => {
   const div = document.createElement("div");
