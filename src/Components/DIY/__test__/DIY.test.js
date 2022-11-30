d9ccf8320afd6e556597259253933e69cc1458d7
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-30 05:21:16 -0500

    Added Unit test for the DIY parts places  method

--- src/Components/DIY/__test__/DIY.test.js
+++ src/Components/DIY/__test__/DIY.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
 import DIY from "../index";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 it("DIY page renders correctly", () => {
   const div = document.createElement("div");
