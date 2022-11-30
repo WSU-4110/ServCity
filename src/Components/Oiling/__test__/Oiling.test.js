2bd1cd2439b1e60d25368934677d1928dc67d1e7
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-30 05:23:56 -0500

    Added Unit test for the Oil change method

--- src/Components/Oiling/__test__/Oiling.test.js
+++ src/Components/Oiling/__test__/Oiling.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
 import Oiling from "../index";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 it("Oil Change page renders correctly", () => {
   const div = document.createElement("div");
