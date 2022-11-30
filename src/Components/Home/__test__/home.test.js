df8fcc8c951c1b446ea0f65fc75b2f85e4f5538d
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-30 05:23:15 -0500

    Added Unit test for the home page method

--- src/Components/Home/__test__/home.test.js
+++ src/Components/Home/__test__/home.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
 import Home from "../index";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 it("Home page renders correctly", () => {
   const div = document.createElement("div");
