6fda79fa57dc25c936c8795f32d5b69c6881ad89
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-30 05:24:43 -0500

    Added Unit test for the Tire Alignment method

--- src/Components/TireAlignment/__test__/TireAlignment.test.js
+++ src/Components/TireAlignment/__test__/TireAlignment.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
 import TireAlignment from "../index";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 it("Tire Aligment page renders correctly", () => {
   const div = document.createElement("div");
