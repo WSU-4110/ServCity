d262f0939629a9c865dcf1ec9ace771b21d8c469
Pollchowdhury <pollchowdhury7@gmail.com>
2022-11-30 05:22:17 -0500

    Added Unit test for the Fluid Flash method

--- src/Components/FluidFlush/__test__/FluidFlash.test.js
+++ src/Components/FluidFlush/__test__/FluidFlash.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
 import Fluidflush from "../index";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 it("Fluid Flash page renders correctly", () => {
   const div = document.createElement("div");
