04865876ef2588a09a89aa74b984b3d732f4ec15
Pollchowdhury <pollchowdhury7@gmail.com>
2022-12-03 20:39:22 -0500

    Tested the insights section

--- src/Components/DIY/index.js
+++ src/Components/DIY/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import InsightsAlignment from "../../InsightsSections/InsightsAlignment";
+import InsightsOilChange from "../../InsightsSections/InsightsOilChange";
+import InsightsTireRotation from "../../InsightsSections/InsightsTireRotation";
 
-function DIYpage(){
-    return(
-        <div>
-            <h1> Welcome to Do it Yourself Page</h1>
-        </div>
-    );
+function DIYpage() {
+  return (
+    <>
+      <div
+        style={{
+          alignItems: "center",
+          display: "block",
+          width: 700,
+          padding: 30,
+        }}
+      >
+        <h2>Alignment Insights</h2>
+        <InsightsAlignment />
+      </div>
+    </>
+  );
 }
 
 export default DIYpage;
--- src/TestInsights.js
+++ src/TestInsights.js
@@ -1,34 +1,51 @@
-import React from 'react'
+import React from "react";
 import "antd/dist/antd.css";
-import InsightsAlignment from './InsightsSections/InsightsAlignment';
-import InsightsOilChange from './InsightsSections/InsightsOilChange'
-import InsightsTireRotation from './InsightsSections/InsightsTireRotation';
+import InsightsAlignment from "./InsightsSections/InsightsAlignment";
+import InsightsOilChange from "./InsightsSections/InsightsOilChange";
+import InsightsTireRotation from "./InsightsSections/InsightsTireRotation";
 
-import Player from './DoItYourself/DoItYourself';
-
-
-
-export default function App() {
-  
+import Player from "./DoItYourself/DoItYourself";
+function App() {
   return (
     <>
-    {/*Calling the Alignment Insights Component*/}
-    <div style={{ alignItems: "center", display: 'block', width: 700, padding: 30 }}>
-      <h2>Alignment Insights</h2>
-      <InsightsAlignment/>
-    </div>
+      {/*Calling the Alignment Insights Component*/}
+      <div
+        style={{
+          alignItems: "center",
+          display: "block",
+          width: 700,
+          padding: 30,
+        }}
+      >
+        <h2>Alignment Insights</h2>
+        <InsightsAlignment />
+      </div>
 
-    {/*Calling the Oil Change Insights Component*/}
-    <div style={{ alignItems: "center", display: 'block', width: 700, padding: 30 }}>
-      <h2>Oil Change Insights</h2>
-      <InsightsOilChange/>
-    </div>
+      {/*Calling the Oil Change Insights Component*/}
+      <div
+        style={{
+          alignItems: "center",
+          display: "block",
+          width: 700,
+          padding: 30,
+        }}
+      >
+        <h2>Oil Change Insights</h2>
+        <InsightsOilChange />
+      </div>
 
-    {/*Calling the Tire Rotation Insights Component*/}
-    <div style={{ alignItems: "center", display: 'block', width: 700, padding: 30 }}>
-      <h2>Tire Rotaiton Insights</h2>
-      <InsightsTireRotation/>
-    </div>
+      {/*Calling the Tire Rotation Insights Component*/}
+      <div
+        style={{
+          alignItems: "center",
+          display: "block",
+          width: 700,
+          padding: 30,
+        }}
+      >
+        <h2>Tire Rotaiton Insights</h2>
+        <InsightsTireRotation />
+      </div>
     </>
   );
 }
